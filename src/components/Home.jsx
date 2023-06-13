84a7ab13b43cac63ba5e0c71720c16a5525f475b
Jaskaran <jskrn.qwerty@gmail.com>
2023-06-13 19:56:29 -0400

    log stats

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,98 +1,97 @@
 import * as XLSX from "xlsx";
 import { utils } from "xlsx";
 import { newEgg_ComputerHardware_BatchItemCreation_ProcessorsDesktops as keywordsList } from "../data/Data";
+import { useState } from "react";
 
 const Home = () => {
-  // const [fileStatus, setFileStatus] = useState("Awaiting file upload");
+  // const [found, setFound] = useState(0);
   // const [isProductFound, setIsProductFound] = useState(false);
   let productsInventory = []; // stores inventory file's data in JSON array
   let productsLineNumbers = []; // list of resp. line numbers of products in inventory file
   let productsFound = []; // stores list of products data gathered from inventory file
   let imageUrls = []; // stores list of image urls of products found
   let keywordsFound = []; // stores list of keywords found in inventory file
-  let keywordsNotFound = []; // stores list of keywords not found in inventory file
-  let isProductFound;
+  let isProductFound = false;
+  // let inventoryCount = 0;
+  let foundCount = 0;
+  let notFoundCount = 0;
 
   const searchKeywords = keywordsList;
 
-  const insertProductInfo = (
-    columnA,
-    columnB,
-    columnC,
-    columnD,
-    columnE,
-    columnF,
-    columnG,
-    columnH
-  ) => {
-    productsFound = [
-      ...productsFound,
-      [columnA, columnB, columnC, columnD, columnE, columnF, columnG, columnH],
-    ];
-  };
+  const prepareProductsInfo = (searchKeywords, productsInventory) => {
+    const insertProductInfo = (
+      columnA,
+      columnB,
+      columnC,
+      columnD,
+      columnE,
+      columnF,
+      columnG
+    ) => {
+      productsFound = [
+        ...productsFound,
+        [columnA, columnB, columnC, columnD, columnE, columnF, columnG],
+      ];
+    };
 
-  const insertHeadings = () => {
-    insertProductInfo(
-      "Sr. No.",
-      "Line No.",
-      "Search Keyword",
-      "Title",
-      "Manufacturer",
-      "UPC",
-      "MPN",
-      "ImageURL"
-    );
-  };
-  insertHeadings();
-
-  const searchFile = (searchKeywords, productsInventory) => {
-    searchKeywords.forEach((searchKeyword, keywordIndex) => {
-      const serialNumber = keywordIndex + 2;
-      isProductFound = false;
-
-      productsInventory.forEach(
-        (productsInventoryItem, productsInventoryItemIndex) => {
-          const lineNumber = productsInventoryItemIndex + 1;
-          const title = productsInventoryItem[2];
-          const upc = productsInventoryItem[24];
-          const manufacturerPartNumber = productsInventoryItem[26];
-          let manufacturer = "";
-
-          if (title !== undefined && title.includes(searchKeyword)) {
-            isProductFound = true;
-            manufacturer = title.split(" ")[0];
-            console.log("Product found", serialNumber);
-            keywordsFound = [...keywordsFound, searchKeyword];
-            insertProductInfo(
-              serialNumber,
-              lineNumber,
-              searchKeyword,
-              title,
-              manufacturer,
-              upc,
-              manufacturerPartNumber
-            );
-            productsLineNumbers = [...productsLineNumbers, lineNumber];
-          }
-        }
+    const insertHeadings = () => {
+      insertProductInfo(
+        "Sr. No.",
+        "Line No.",
+        "Search Keyword",
+        "Title",
+        "Manufacturer",
+        "UPC",
+        "MPN",
+        "ImageURL"
       );
-      if (!isProductFound) {
-        console.log("Product not found on", serialNumber);
-        insertProductInfo(serialNumber, "", searchKeyword, "", "", "", "");
-      }
-    });
-  };
+    };
 
-  // const listMissingKeywords = (searchKeywords, keywordsFoundArr) => {
-  //   let difference = searchKeywords.filter(
-  //     (keyword) => !keywordsFoundArr.includes(keyword)
-  //   );
-  //   console.log(`${difference.length} products were not found`);
-  //   console.log(difference);
-  //   // missingKeywordsArr = difference;
-  //   keywordsNotFound = difference.map((item) => [item]);
-  //   console.log("missingKeywordsArr", keywordsNotFound);
-  // };
+    const searchAndInsertProductInfo = () => {
+      searchKeywords.forEach((searchKeyword, keywordIndex) => {
+        const serialNumber = keywordIndex + 2;
+        isProductFound = false;
+
+        productsInventory.forEach(
+          (productsInventoryItem, productsInventoryItemIndex) => {
+            const lineNumber = productsInventoryItemIndex + 1;
+            const title = productsInventoryItem[2];
+            const upc = productsInventoryItem[24];
+            const manufacturerPartNumber = productsInventoryItem[26];
+            let manufacturer = "";
+
+            if (title !== undefined) {
+              if (title.includes(searchKeyword)) {
+                isProductFound = true;
+                foundCount += 1;
+                manufacturer = title.split(" ")[0];
+                // console.log("Product found", serialNumber);
+                keywordsFound = [...keywordsFound, searchKeyword];
+                insertProductInfo(
+                  serialNumber,
+                  lineNumber,
+                  searchKeyword,
+                  title,
+                  manufacturer,
+                  upc,
+                  manufacturerPartNumber
+                );
+                productsLineNumbers = [...productsLineNumbers, lineNumber];
+              }
+            }
+          }
+        );
+        if (!isProductFound) {
+          // console.log("Product not found on", serialNumber);
+          insertProductInfo(serialNumber, "", searchKeyword, "", "", "", "");
+          notFoundCount += 1;
+        }
+      });
+    };
+    insertHeadings();
+    searchAndInsertProductInfo();
+    return productsFound;
+  };
 
   const exportNewFile = (arr) => {
     const newWorkbook = XLSX.utils.book_new();
@@ -101,29 +100,55 @@ const Home = () => {
     XLSX.writeFile(newWorkbook, "NewSheet.xlsx");
   };
 
-  const listImageUrlsArr = (prodLine, prodArr) => {
-    prodLine.forEach((lineNum) => {
-      prodArr.forEach((prodItem, index) => {
-        const line = index + 1;
-        const prodImage = prodItem[41];
-        if (lineNum === line - 1) {
+  const findImageUrls = (prod, inventory) => {
+    prod.forEach((prodItem) => {
+      let isImage = false;
+      const prodLineNum = prodItem[1];
+      inventory.forEach((inventoryItem, inventoryItemIndex) => {
+        const inventoryLineNum = inventoryItemIndex + 1;
+        const prodImage = inventoryItem[41];
+        const imageLineNumber = inventoryLineNum + 1;
+        if (prodLineNum === imageLineNumber) {
+          isImage = true;
           imageUrls.push(prodImage);
         }
       });
+      if (!isImage) {
+        imageUrls.push("");
+      }
     });
+    return imageUrls;
   };
 
-  const combineProductsAndImageUrlArr = (productsFoundArr, imageUrlsArr) => {
-    productsFoundArr.map((productItem, productIndex) => {
+  const combine = (products, imageUrlsArr) => {
+    products.map((productItem, productIndex) => {
       imageUrlsArr.forEach((imageItem, imageIndex) => {
         if (productIndex === imageIndex) {
           productItem.push(imageItem);
         }
       });
     });
+    return products;
   };
 
-  // const insertMissingKeywordsToProductsFoundArr = () => {};
+  const calculateStats = (
+    searchKeywords,
+    productsInventory,
+    products,
+    finalProducts
+  ) => {
+    const duplicates = foundCount + notFoundCount - searchKeywords.length;
+    console.log("---Export Stats---");
+    console.log("Total lines searched:", productsInventory.length);
+    console.log("Searching:", searchKeywords.length);
+    console.log("Listed:", finalProducts.length - 1);
+    console.log(
+      "Duplicates:",
+      foundCount + notFoundCount - searchKeywords.length
+    );
+    console.log("Found:", foundCount - duplicates);
+    console.log("Not found:", notFoundCount);
+  };
 
   // ----------------------------------------------------------------------
   // handle functions
@@ -131,7 +156,7 @@ const Home = () => {
 
   const readFileData = async (e) => {
     // setFileStatus("Reading inventory file"); // this breaks the code
-    console.log("Reading inventory file");
+    console.log("reading inventory file...");
     const file = e.target.files[0];
     const fileData = await file.arrayBuffer();
     const workbook = XLSX.read(fileData);
@@ -145,25 +170,20 @@ const Home = () => {
     const source = e.target.name;
     if (source === "inventoryFile") {
       productsInventory = jsonDataArr;
-      console.log("File read");
+      console.log("File read!");
       console.log("Ready for data extraction");
     }
     // setFileStatus("Inventory file read"); // this breaks the code
   };
 
-  const filterAndExportData = () => {
-    searchFile(searchKeywords, productsInventory);
+  const handleExportButton = () => {
+    const products = prepareProductsInfo(searchKeywords, productsInventory);
     const indexNumList = productsInventory[0];
     console.log(indexNumList);
-    console.log(
-      `${productsLineNumbers.length} out of ${searchKeywords.length} items were found`
-    );
-    console.log(productsFound);
-    // listMissingKeywords(searchKeywords, keywordsFound);
-    listImageUrlsArr(productsLineNumbers, productsInventory);
-    combineProductsAndImageUrlArr(productsFound, imageUrls);
-    // insertMissingKeywordsToProductsFoundArr(keywordsNotFound);
-    exportNewFile(productsFound.concat(keywordsNotFound));
+    const productsImageUrls = findImageUrls(products, productsInventory);
+    const finalProducts = combine(products, productsImageUrls);
+    // exportNewFile(finalProducts);
+    calculateStats(searchKeywords, productsInventory, products, finalProducts);
   };
 
   return (
@@ -184,10 +204,11 @@ const Home = () => {
         </p> */}
         <button
           type="button"
-          onClick={filterAndExportData}
+          onClick={handleExportButton}
         >
           Extract Data
         </button>
+        {/* <p>Search items: {toBeFound}</p> */}
       </div>
     </>
   );
